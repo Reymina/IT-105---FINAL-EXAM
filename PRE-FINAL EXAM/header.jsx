a9c9b94f677ba0821284bd36ec8c4d998a2e0972
Reymina <105359612+Reymina@users.noreply.github.com>
2022-05-12 03:03:01 -0700

    Add files via upload

---
+++ PRE-FINAL EXAM/header.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Header() {
+  return <header style={{background: "pink", color: "black"}}><h1>My todo list</h1></header>;
+}
+
+export default Header;
---
+++ PRE-FINAL EXAM/note.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Note() {
+  return <div style={{background: "yellow", color: "black"}}><h1>My Todo List</h1></div>;
+}
+
+export default Note;
